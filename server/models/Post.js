15c7219dc458ef4ab4dbd50a4565349099335646
LuchiBobra7 <46483435+LuchiBobra7@users.noreply.github.com>
2019-01-16 15:44:17 +0200

    Create Post.js

---
+++ server/models/Post.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model("Post", PostSchema);
